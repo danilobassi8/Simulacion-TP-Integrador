a24632a066d7c931f428226629d4cdf340b97616
elcurco8 <danilo-bassi@hotmail.com>
2019-08-11 17:31:23 -0300

    Avance en la estructura del algoritmo
    Bastante avance en el algoritmo. en teoría estaria completo. hay que depurarlo.

--- Sim-TPI/PythonApplication1/PythonApplication1.py
+++ Sim-TPI/PythonApplication1/PythonApplication1.py
@@ -8,6 +8,7 @@ def Inicializacion():
     global server1,server2,server3,server4,server5
     global cola1,cola2,cola3
     global contadorSistema
+    global demoraTotal,util_sv1,util_sv2,util_sv3,util_sv4,util_sv5
 
 
     #creo los servidores y las colas.
@@ -19,7 +20,7 @@ def Inicializacion():
 
     server1.tasa = 7
     server2.tasa = 5
-    server3.tasa = 5
+    server3.tasa = 5        #en clientes/unidad de tiempo
     server4.tasa = 5
     server5.tasa = 6
 
@@ -52,6 +53,7 @@ class Servidor():
         self.tasa = 0
         self.cliente = 0
         self.colaAsociada = 0
+        self.banderaUtilizacion = 0
 
     def VaciaServer():
         pass
@@ -64,8 +66,15 @@ class Cola():
         self.clientes = []
         self.politicaAtencion = "politica no asignada."
 
-    def DameCliente():
-        pass
+    def DameCliente(self):
+        if(self.politicaAtencion == "FIFO"):
+            retorno = self.clientes[0]
+            self.clientes.remove(retorno)
+            return retorno
+        elif(self.politicaAtencion == "LIFO"):
+            retorno = self.clientes[len(clientes) - 1]
+            self.clientes.remove(retorno)
+            return retorno
 
 class Cliente():
     def __init__(self):
@@ -103,30 +112,176 @@ def proximoEvento():
         return "partida4"
     elif(ListaEventos.partida5 == numeroMinimo):
         return "partida5"
+    # # # # este método solo determina cual va a ser el proximo evento # # #
+    return 0
 
+def generarPrioridad(porcentaje):
+    r = random.uniform(0,1)
+    if(r * 100 <= porcentaje):
+        return True
+    else:
+        return False
 
 def OcurreProximoEvento(nextEvent):
+    global clock
+    global server1,server2,server3,server4,server5
+    global cola1,cola2,cola3
+    global demoraTotal,util_sv1,util_sv2,util_sv3,util_sv4,util_sv5
+
     if(nextEvent == "llegada"): 
-        pass
+        #avanzo el reloj y creo la proxima llegada.
+        clock = ListaEventos.llegadaProximoCliente
+        ListaEventos.llegadaProximoCliente = generarAleatorioExponencial(1 / 10) + clock
+
+        #creo un cliente con su tiempo de llegada y prioridad.
+        clienteNuevo = Cliente()
+        clienteNuevo.tiempoArribo = clock
+        clienteNuevo.prioridad = generarPrioridad(5)
+
+        #meto el cliente en alguna de las dos colas.
+        if(len(cola1.clientes) <= len(cola2.clientes)):
+            cola1.clientes.append(clienteNuevo)
+        else:
+            cola2.clientes.append(clienteNuevo)
+                    
     elif(nextEvent == "partida1"):
-        pass
+        #actualizo el clock
+        clock = ListaEventos.partida1
+
+        clienteSV1 = server1.cliente
+        server1.cliente = 0
+        cola3.clientes.append(clienteSV1)
+        #calculo el tiempo utilizado del servidor en base a la bandera que se
+        #actualiza cuando entra un cliente.
+        util_sv1 = util_sv1 + clock - server1.banderaUtilizacion
+
     elif(nextEvent == "partida2"):
-        pass
+        #actualizo el clock
+        clock = ListaEventos.partida2
+
+        clienteSV2 = server2.cliente
+        server2.cliente = 0
+        cola3.clientes.append(clienteSV2)
+        #calculo el tiempo utilizado del servidor en base a la bandera que se
+        #actualiza cuando entra un cliente.
+        util_sv2 = util_sv2 + clock - server2.banderaUtilizacion
+
     elif(nextEvent == "partida3"):
-        pass
+        #actualizo el clock
+        clock = ListaEventos.partida3
+
+        clienteSV3 = server3.cliente
+        server3.cliente = 0
+        
+        #calculo el tiempo utilizado del servidor en base a la bandera que se
+        #actualiza cuando entra un cliente.
+        util_sv3 = util_sv3 + clock - server3.banderaUtilizacion
+
+        #sumo a la demora total del sistema (clock - tiempollegada del cliente)
+        demoraTotal = demoraTotal + clock - clienteSV3.tiempoArribo
+
     elif(nextEvent == "partida4"):
-        pass
+        clock = ListaEventos.partida4
+
+        clienteSV4 = server4.cliente
+        server4.cliente = 0
+        
+        #calculo el tiempo utilizado del servidor en base a la bandera que se
+        #actualiza cuando entra un cliente.
+        util_sv4 = util_sv4 + clock - server4.banderaUtilizacion
+
+        #sumo a la demora total del sistema (clock - tiempollegada del cliente)
+        demoraTotal = demoraTotal + clock - clienteSV4.tiempoArribo
     elif(nextEvent == "partida5"):
-        pass
+        clock = ListaEventos.partida5
+
+        clienteSV5 = server5.cliente
+        server5.cliente = 0
+        
+        #calculo el tiempo utilizado del servidor en base a la bandera que se
+        #actualiza cuando entra un cliente.
+        util_sv5 = util_sv5 + clock - server5.banderaUtilizacion
+
+        #sumo a la demora total del sistema (clock - tiempollegada del cliente)
+        demoraTotal = demoraTotal + clock - clienteSV5.tiempoArribo
+
+def GestionDeServidores():
+    global clock
+    global server1,server2,server3,server4,server5
+    global cola1,cola2,cola3
+
+    if(server1.cliente == 0):
+        if(len(cola1.clientes) > 0):
+            server1.cliente = cola1.DameCliente()
+            server1.banderaUtilizacion = clock
+            ListaEventos.partida1 = generarAleatorioExponencial(1 / server1.tasa) + clock
+        else:
+            ListaEventos.partida1 = 99999999999999999999999 * 99999999999999999999999
+     #de aca para abajo es copy paste con su correspondiente cola y servidor.
+    if(server2.cliente == 0):
+        if(len(cola2.clientes) > 0):
+            server2.cliente = cola2.DameCliente()
+            server2.banderaUtilizacion = clock
+            ListaEventos.partida2 = generarAleatorioExponencial(1 / server2.tasa) + clock
+        else:
+            ListaEventos.partida2 = 99999999999999999999999 * 99999999999999999999999
+    if(server3.cliente == 0):
+        if(len(cola3.clientes) > 0):
+            server3.cliente = cola3.DameCliente()
+            server3.banderaUtilizacion = clock
+            ListaEventos.partida3 = generarAleatorioExponencial(1 / server3.tasa) + clock
+        else:
+            ListaEventos.partida3 = 99999999999999999999999 * 99999999999999999999999
+    if(server4.cliente == 0):
+        if(len(cola3.clientes) > 0):
+            server4.cliente = cola3.DameCliente()
+            server4.banderaUtilizacion = clock
+            ListaEventos.partida4 = generarAleatorioExponencial(1 / server4.tasa) + clock
+        else:
+            ListaEventos.partida4 = 99999999999999999999999 * 99999999999999999999999
+    if(server5.cliente == 0):
+        if(len(cola3.clientes) > 0):
+            server5.cliente = cola3.DameCliente()
+            server5.banderaUtilizacion = clock
+            ListaEventos.partida5 = generarAleatorioExponencial(1 / server5.tasa) + clock
+        else:
+            ListaEventos.partida5 = 99999999999999999999999 * 99999999999999999999999
+    return 0
+
+
+def Graficar():
+    global clock
+    global server1,server2,server3,server4,server5
+    global cola1,cola2,cola3
+    global contadorSistema
+    global demoraTotal,util_sv1,util_sv2,util_sv3,util_sv4,util_sv5
+
+    print("--------------  " + str(contadorSistema) +"  ---------------")
+    print()
+    print("cola1: ", end = '')
+    print(cola1.clientes)
+    print("                                                  server1: " , end = '')
+    print([server1.cliente])
+    print("cola2: ", end = '')
+    print(cola2.clientes)
+    print("                                                  server2: " , end = '')
+    print([server2.cliente])
+
+
 
 
 
 Inicializacion()
 sigueSimulacion = True
+Graficar()
+contadorSistema = 0
 while(sigueSimulacion):
     nextEvent = proximoEvento() #basicamente se fija en cual es el evento mas proximo y lo devuelve.
-    
     OcurreProximoEvento(nextEvent)
+    Graficar()
+    GestionDeServidores()
+    Graficar()
+    contadorSistema = contadorSistema+1
     
    
 
